5a87a60ba619a96d4abfb70aade3e7f427156c41
akshayhp2298 <akshayhp2298@gmail.com>
2019-12-10 17:18:16 +0530

    changes for deploy

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const app = express();
-const route = require('./Routes')
-app.use(route);
-app.listen(3000);
+require("dotenv")
+const express = require("express")
+const app = express()
+const route = require("./Routes")
+const PORT = process.env.PORT
+app.use(route)
+app.listen(PORT, err => {
+  if (err) {
+    console.log("Error occurred while starting the server")
+    console.log(err)
+  }
+  console.log("Server running at post: ", PORT)
+})
